8c31cf85143d7e1f54c56e881d33573b79b9d6a7
AnonymAsimov <anton.choi92@gmail.com>
2024-10-03 14:11:03 -0400

    ㅁ
    ㅁ

--- assignment/04-week/external.js
+++ assignment/04-week/external.js
@@ -1,56 +1,61 @@
+// JSON 데이터 로드
 var jsonData = d3.json("anscombe.json");
 
+Promise.all([jsonData])
+    .then(function(data) {
+        // 데이터 확인 (Console에 출력)
+        console.log(data);
 
-Promise.all([jsonData]).then(function(data){
-    console.log(data);
-    var dataset = data[0];
-    
-    scatterPlot(dataset["set1"]);
-    
-    barChart(dataset["set1"]);
-});
+        // 데이터 구조 접근: data[0]으로 JSON 내부 데이터 접근 가능
+        scatterPlot(data[0]["set1"]); // 첫 번째 데이터셋을 사용하여 산점도 그리기
+        barChart(data[0]["set2"]); // 두 번째 데이터셋을 사용하여 막대 차트 그리기
+    })
+    .catch(function(error) {
+        console.error("Error loading the JSON file: ", error);
+    });
 
+// Scatter Plot 함수 정의
 function scatterPlot(inputDataset) {
-    var svg = d3.select("body")
-       .append("svg")
-       .attr("width", 400)
-       .attr("height", 400);
+    var svg = d3.select("#scatterPlot")
+        .append("svg")
+        .attr("width", 400)
+        .attr("height", 400);
 
     var xScale = d3.scaleLinear()
-       .domain([0, 20])
-       .range([0, 300]);
+        .domain([0, 20])
+        .range([0, 300]);
 
     var yScale = d3.scaleLinear()
-       .domain([0, 20])
-       .range([300, 0]);
+        .domain([0, 20])
+        .range([300, 0]);
 
     svg.selectAll("circle")
-       .data(inputDataset)
-       .enter()
-       .append("circle")
-       .attr("r", 5)
-       .attr("cx", function(d) { return xScale(d.x); })
-       .attr("cy", function(d) { return yScale(d.y); })
-       .attr("transform", "translate(30,30)");
+        .data(inputDataset)
+        .enter()
+        .append("circle")
+        .attr("r", 5)
+        .attr("cx", function(d) { return xScale(d.x); })
+        .attr("cy", function(d) { return yScale(d.y); })
+        .attr("transform", "translate(30,30)");
 
     var xAxis = d3.axisBottom().scale(xScale);
-    svg.append("g").call(xAxis).attr("transform", "translate(30,330)");
-
     var yAxis = d3.axisLeft().scale(yScale);
-    svg.append("g").call(yAxis).attr("transform", "translate(30,30)");
 
+    svg.append("g").call(xAxis).attr("transform", "translate(30,330)");
+    svg.append("g").call(yAxis).attr("transform", "translate(30,30)");
     svg.append("text").text("x").attr("x", 160).attr("y", 360);
     svg.append("text").text("y").attr("x", 0).attr("y", 160);
 }
 
+// Bar Chart 함수 정의
 function barChart(inputDataset) {
-    var svg = d3.select("body")
+    var svg = d3.select("#barChart")
         .append("svg")
         .attr("width", 400)
         .attr("height", 400);
 
     var xScale = d3.scaleBand()
-        .domain(d3.range(inputDataset.length))
+        .domain(inputDataset.map(function(d, i) { return i; })) // Index를 X 축 도메인으로 사용
         .range([0, 300])
         .padding(0.1);
 
@@ -66,15 +71,12 @@ function barChart(inputDataset) {
         .attr("y", function(d) { return yScale(d.y); })
         .attr("width", xScale.bandwidth())
         .attr("height", function(d) { return 300 - yScale(d.y); })
-        .attr("fill", "#69b3a2")
+        .attr("fill", "steelblue")
         .attr("transform", "translate(30,30)");
 
     var xAxis = d3.axisBottom().scale(xScale);
-    svg.append("g").call(xAxis).attr("transform", "translate(30,330)");
-
     var yAxis = d3.axisLeft().scale(yScale);
-    svg.append("g").call(yAxis).attr("transform", "translate(30,30)");
 
-    svg.append("text").text("Index").attr("x", 160).attr("y", 360);
-    svg.append("text").text("Value (y)").attr("x", 0).attr("y", 160);
+    svg.append("g").call(xAxis).attr("transform", "translate(30,330)");
+    svg.append("g").call(yAxis).attr("transform", "translate(30,30)");
 }
